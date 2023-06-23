f52e4aad5b7187aa77df6e6028e2f33845c85bf6
birahnu-moges <birhanu18@gmail.com>
2023-06-23 07:31:48 -0700

    tadk 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install puppet-lint
-package { 'puppet-lint':
-  ensure          =>  'installed',
-  install_options =>  ['-v', '2.1.1'],
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
